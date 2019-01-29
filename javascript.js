52ec0954695f984fdb5ee9c5ee8f6af369793e54
gackey <gackey@Mac-mini.local>
2019-01-29 13:03:26 +0900

    <li>仮対応

--- javascript.js
+++ javascript.js
@@ -1,24 +1,25 @@
 //ここにスキンで利用するJavaScriptを記入する
 
-/* #main内にある半角英数字記号文字(列)にspan */
-//alert('html');
+/* .article内の<p>、<li>にある半角英数字記号文字(列)にspan */
 jQuery(function(){
-  jQuery("p").each(function() {
-    var txt = jQuery(this).text();
+  jQuery('.article p,.article li[class != "blocks-gallery-item"]').each(function() {
     jQuery(this).html(
-      txt.replace(
-        /[\s!-;=-~]+/g,
-        '<span class = "thx_q_space">$&</span>'
+      jQuery(this).html().replace(
+        /(<\/?[^>]+>)|([\s!-;=-~]+)/g,
+        function(){
+          if( arguments[1] ) return arguments[1] ;
+          if( arguments[2] ) return '<span class = "thx_wao">' + arguments[2] + '</span>' ;
+        }
       )
     );
   });
 });
 
-/* スライドコンテンツ
+/* スライドコンテンツ*/
 jQuery(function(){
   jQuery('[data-slide-contents]').click(function(event) {
     event.preventDefault();
     var slideContents = jQuery(this).data('slide-contents');
     jQuery('#' + slideContents).slideToggle(400);
   });
-});*/
+});
