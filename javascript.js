c41f98e86a3449f83ebe20c91c354061baea432b
gackey <gackey@Mac-mini.local>
2019-02-05 12:55:14 +0900

    行取り、.wp-block-spacer追加

--- javascript.js
+++ javascript.js
@@ -36,7 +36,7 @@ jQuery(function(){
   var fz = parseInt(jQuery('body').css('font-size'));
   var lh = parseInt(jQuery('body').css('line-height'));
   var ls = lh - fz;
-  var lineUpOnce = jQuery(`pre,.blogcard-content,blockquote`);
+  var lineUpOnce = jQuery(`pre,.blogcard-content,blockquote,.wp-block-spacer`);
   var lineUp = jQuery(`
     table,
     .ad-area,
