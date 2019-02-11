4cb21a01ce3ce624cfe3b5193769161916b29be7
gackey <gackey@Mac-mini.local>
2019-02-11 12:49:09 +0900

    get_thx_categories　に　cat-label　追加

--- functions.php
+++ functions.php
@@ -157,7 +157,7 @@ function get_thx_categories(){
   $categories = null;
   $categories = '<div class="thx_category_space">';
   foreach((get_the_category()) as $category){
-    $categories .= '<div class="entry-category thx-label cat-label-'.$category->cat_ID.'">'.$category->cat_name.'</div>';
+    $categories .= '<div class="entry-category thx-label cat-label cat-label-'.$category->cat_ID.'">'.$category->cat_name.'</div>';
   }
   $categories .= '</div>';
   return $categories;
