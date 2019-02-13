425e977e4aca59e6ab16e14d79de31ffff08dd5d
gackey <gackey@Mac-mini.local>
2019-02-13 16:40:37 +0900

    記述を変数化

--- cocoon-child-master/thx/css-custom.php
+++ cocoon-child-master/thx/css-custom.php
@@ -20,18 +20,6 @@ if (get_site_key_sub_color()){
   $key_sub_color = colorcode_to_hsl_css_code($key_color,1.4);
 }?>
 
-<?php //thx ?>
-.entry-title,
-.archive-title {
-  color: #fff;
-  background-color: <?php echo $key_color; ?>;
-}
-.article h3 {
-  background: linear-gradient(90deg, <?php echo colorcode_to_hsl_css_code($key_color,1.4); ?>, #fff);
-}
-
-
-
 
 
 <?php //base ?>
@@ -45,28 +33,38 @@ if (get_site_key_sub_color()){
 .pagination-next-link,
 .comment-reply-link,
 .toc{
-  border: 1px solid <?php echo get_site_key_sub_color(); ?>;
+  border: 1px solid <?php echo $key_sub_color; ?>;
 }
 .pagination .current,
 .page-numbers.current,
 .page-numbers.dots{
-  background: <?php echo get_site_key_sub_color(); ?>;
+  background: <?php echo $key_sub_color; ?>;
 }
 .comment-list{
   ul.children{
     padding: 0.6em;
     background-color: <?php echo colorcode_to_rgb_css_code(get_site_key_color(), 0.95); ?>;
-    border-left: 2px solid <?php echo get_site_key_sub_color(); ?>;
+    border-left: 2px solid <?php echo $key_sub_color; ?>;
   }
 }
 
+
+
 <?php //thx ?>
+.entry-title,
+.archive-title {
+  color: #fff;
+  background-color: <?php echo $key_color; ?>;
+}
+.article h3 {
+  background: linear-gradient(90deg, <?php echo colorcode_to_hsl_css_code($key_color,1.4); ?>, #fff);
+}
 .article h2::after,
 .article h3::after,
 .article h4::after,
 .article h5::after,
 .article h6::after {
-  color: <?php echo colorcode_to_rgb_css_code(get_site_key_sub_color(), 0.5); ?>;
+  color: <?php echo colorcode_to_rgb_css_code($key_sub_color, 0.5); ?>;
 }
 
 .article h2:hover::after,
@@ -74,9 +72,9 @@ if (get_site_key_sub_color()){
 .article h4:hover::after,
 .article h5:hover::after,
 .article h6:hover::after {
-	color: <?php echo colorcode_to_rgb_css_code(get_site_key_sub_color(), 0.0); ?>;
+	color: <?php echo colorcode_to_rgb_css_code($key_sub_color, 0.0); ?>;
 }
 
 .go-to-top-button{
-  background-color: <?php echo get_site_key_sub_color(); ?>;
+  background-color: <?php echo $key_sub_color; ?>;
 }
