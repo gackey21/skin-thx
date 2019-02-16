f9c52bda6933041130a09cfbf8aa6897d1383721
gackey <gackey@Mac-mini.local>
2019-02-16 13:14:01 +0900

    get_thx_categories()　削除

--- functions.php
+++ functions.php
@@ -23,27 +23,6 @@ function is_entry_card_border_visible(){
 
 
 
-
-/*
-//リンクのないカテゴリーの取得（複数）
-//カテゴリーID付加
-if ( !function_exists( 'get_thx_categories' ) ):
-function get_thx_categories(){
-  $categories = null;
-  $categories = '<div class="thx_category_space">';
-  foreach((get_the_category()) as $category){
-    $categories .= '<div class="entry-category thx-label cat-label cat-label-'.$category->cat_ID.'">'.$category->cat_name.'</div>';
-  }
-  $categories .= '</div>';
-  return $categories;
-}
-endif;
-
-if ( !function_exists( 'thx_categories' ) ):
-function thx_categories(){
-  echo get_thx_categories();
-}
-endif;*/
 get_template_part('skins/skin-thx/lib/thx','hsla');
 
 
