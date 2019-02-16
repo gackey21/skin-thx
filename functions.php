952d555adf15f291ce47e3bf53bff7dbe509b27b
gackey <gackey@Mac-mini.local>
2019-02-16 14:26:06 +0900

    etc　ファイル分割

--- functions.php
+++ functions.php
@@ -23,52 +23,9 @@ function is_entry_card_border_visible(){
 
 
 
+get_template_part('skins/skin-thx/lib/thx','etc');
 get_template_part('skins/skin-thx/lib/thx','hsla');
 get_template_part('skins/skin-thx/lib/thx','cat-label');
 get_template_part('skins/skin-thx/lib/thx','amp');
 get_template_part('skins/skin-thx/lib/thx','anime');
 get_template_part('skins/skin-thx/lib/thx','test');
-
-
-
-
-
-
-
-// 引用符解除
-remove_filter("the_content", "wptexturize");
-remove_filter("the_excerpt", "wptexturize");
-remove_filter("the_title", "wptexturize");
-
-
-
-
-
-
-
-/* JS非同期読み込み
-if (!(is_admin() )) {
-function add_defer_to_enqueue_script( $url ) {
-    if ( FALSE === strpos( $url, '.js' ) ) return $url;
-    if ( strpos( $url, 'jquery.min.js' ) ) return $url;
-    if ( strpos( $url, 'highlight.min.js' ) ) return $url;
-    return "$url' defer charset='UTF-8";
-}
-add_filter( 'clean_url', 'add_defer_to_enqueue_script', 11, 1 );
-}*/
-
-
-/* カスタムフィールドでショートコード */
-echo apply_filters('the_content', get_post_meta($post->ID, 'custom_field', true));
-
-
-
-/* <body>にクラス追加 */
-add_filter('body_class', 'thx_body_class_additional');
-if ( !function_exists( 'thx_body_class_additional' ) ):
-function thx_body_class_additional($classes) {
-  global $post;
-  $classes[] = 'thx';
-  return apply_filters('thx_body_class_additional', $classes);
-}//body_class_additional
-endif;
---
+++ lib/thx-etc.php
@@ -0,0 +1,34 @@
+<?php
+
+// 引用符解除
+remove_filter("the_content", "wptexturize");
+remove_filter("the_excerpt", "wptexturize");
+remove_filter("the_title", "wptexturize");
+
+// <body>にクラス追加
+add_filter('body_class', 'thx_body_class_additional');
+if ( !function_exists( 'thx_body_class_additional' ) ):
+function thx_body_class_additional($classes) {
+  global $post;
+  $classes[] = 'thx';
+  return apply_filters('thx_body_class_additional', $classes);
+}//body_class_additional
+endif;
+
+// カスタムフィールドでショートコード
+echo apply_filters('the_content', get_post_meta($post->ID, 'custom_field', true));
+
+
+
+/* JS非同期読み込み
+if (!(is_admin() )) {
+function add_defer_to_enqueue_script( $url ) {
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.min.js' ) ) return $url;
+    if ( strpos( $url, 'highlight.min.js' ) ) return $url;
+    return "$url' defer charset='UTF-8";
+}
+add_filter( 'clean_url', 'add_defer_to_enqueue_script', 11, 1 );
+}*/
+
+ ?>
