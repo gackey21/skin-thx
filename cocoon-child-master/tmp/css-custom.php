ce93e8cd2fe8a8a0246cd5b900e592b0ff3ce2f6
gackey <gackey@Mac-mini.local>
2019-02-16 14:53:10 +0900

    require　のみ

---
+++ cocoon-child-master/tmp/css-custom.php
@@ -0,0 +1,11 @@
+<?php //CSS設定用
+/**
+ * Cocoon WordPress Theme
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+if ( !defined( 'ABSPATH' ) ) exit; ?>
+
+<?php require get_template_directory().'/tmp/css-custom.php'; ?>
+<?php require get_stylesheet_directory().'/skins/skin-thx/tmp/css-custom.php'; ?>
