ffeb4f08ef53a687f89adfd543f8b56284bdd5a7
gackey <gackey@Mac-mini.local>
2019-02-17 17:11:23 +0900

    打ち消し類　ファイル分割

--- tmp/css_php/_css-custom.php
+++ tmp/css_php/_css-custom.php
@@ -10,36 +10,5 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
 <?php //require_once get_template_directory().'/tmp/css-custom.php'; ?>
 
 <?php require '_var.php'; ?>
+<?php require 'deicide.php'; ?>
 <?php require 'color.php'; ?>
-
-
-
-<?php //打ち消し ?>
-.entry-content > *,
-.demo .entry-content p {
-  line-height: <?php echo $thx_lh_px; ?>px;
-}
-.entry-content > *,
-.demo .entry-content p {
-  margin-top: <?php echo $thx_ls; ?>px;
-  margin-bottom: <?php echo $thx_ls; ?>px;
-}
-.a-wrap:hover {
-  background-color:initial;
-}
-.blogcard-wrap:hover {
-  background-color:#fff;
-}
-
-
-<?php //管理画面用
-if(is_admin() && is_gutenberg_editor_enable()) ?>
-.main p,
-.main p.wp-block-paragraph {
-  line-height: <?php echo $thx_lh_px; ?>px;
-}
-.entry-content > *,
-.demo .entry-content p {
-  margin-top: <?php echo $thx_ls; ?>px;
-  margin-bottom: <?php echo $thx_ls; ?>px;
-}
---
+++ tmp/css_php/deicide.php
@@ -0,0 +1,37 @@
+<?php
+///////////////////////////////////////
+// 打ち消し
+///////////////////////////////////////
+?>
+.entry-content > *,
+.demo .entry-content p {
+  line-height: <?php echo $thx_lh_px; ?>px;
+}
+.entry-content > *,
+.demo .entry-content p {
+  margin-top: <?php echo $thx_ls; ?>px;
+  margin-bottom: <?php echo $thx_ls; ?>px;
+}
+.a-wrap:hover {
+  background-color:initial;
+}
+.blogcard-wrap:hover {
+  background-color:#fff;
+}
+
+<?php
+///////////////////////////////////////
+// 管理画面用
+///////////////////////////////////////
+?>
+<?php
+if(is_admin() && is_gutenberg_editor_enable()) ?>
+.main p,
+.main p.wp-block-paragraph {
+  line-height: <?php echo $thx_lh_px; ?>px;
+}
+.entry-content > *,
+.demo .entry-content p {
+  margin-top: <?php echo $thx_ls; ?>px;
+  margin-bottom: <?php echo $thx_ls; ?>px;
+}
