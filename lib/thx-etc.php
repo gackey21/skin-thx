586af998fb3e9e50eff7548e99b526559548e36b
gackey <gackey@Mac-mini.local>
2019-02-18 10:35:21 +0900

    thx_enqueue_styles()、is_mobile()　追加

--- lib/thx-etc.php
+++ lib/thx-etc.php
@@ -5,6 +5,9 @@ remove_filter("the_content", "wptexturize");
 remove_filter("the_excerpt", "wptexturize");
 remove_filter("the_title", "wptexturize");
 
+// カスタムフィールドでショートコード
+echo apply_filters('the_content', get_post_meta($post->ID, 'custom_field', true));
+
 // <body>にクラス追加
 add_filter('body_class', 'thx_body_class_additional');
 if ( !function_exists( 'thx_body_class_additional' ) ):
@@ -15,10 +18,32 @@ function thx_body_class_additional($classes) {
 }//body_class_additional
 endif;
 
-// カスタムフィールドでショートコード
-echo apply_filters('the_content', get_post_meta($post->ID, 'custom_field', true));
+// スマホ判別
+function is_mobile() {
+    $useragents = array(
+        'iPhone',          // iPhone
+        'iPod',            // iPod touch
+        '^(?=.*Android)(?=.*Mobile)', // 1.5+ Android
+        'dream',           // Pre 1.5 Android
+        'CUPCAKE',         // 1.5+ Android
+        'blackberry9500',  // Storm
+        'blackberry9530',  // Storm
+        'blackberry9520',  // Storm v2
+        'blackberry9550',  // Storm v2
+        'blackberry9800',  // Torch
+        'webOS',           // Palm Pre Experimental
+        'incognito',       // Other iPhone browser
+        'webmate'          // Other iPhone browser
+    );
+    $pattern = '/'.implode('|', $useragents).'/i';
+    return preg_match($pattern, $_SERVER['HTTP_USER_AGENT']);
+}
 
-
+// style.php読み込み
+function thx_enqueue_styles() {
+  wp_enqueue_style( 'thx-style', get_stylesheet_directory_uri().'/skins/skin-thx/tmp/css_php/style.php' );
+}
+add_action( 'wp_enqueue_scripts', 'thx_enqueue_styles' );
 
 /* JS非同期読み込み
 if (!(is_admin() )) {
