0efda313be2c47cf20bc78a3ee220113a5aea974
gackey <gackey@Mac-mini.local>
2019-02-18 19:19:09 +0900

    おまじない

--- tmp/css_php/style.php
+++ tmp/css_php/style.php
@@ -3,5 +3,6 @@ include_once(__DIR__. '/../../../../../../../wp-load.php');
 header('Content-Type: text/css; charset=utf-8');
 //include_once('_var.php');
 ?>
+@charset "UTF-8";
 <?php require '_var.php'; ?>
 <?php require 'set-grid.php'; ?>
